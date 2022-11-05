02ab385d3f6eed11b09aaf561e994d2b7c19ab27
Omico <me@omico.icu>
2022-11-05 03:59:20 -0700

    core: ui: theme: Use theme builder

--- core/ui/theme/src/main/kotlin/me/omico/xero/ui/theme/Theme.kt
+++ core/ui/theme/src/main/kotlin/me/omico/xero/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package me.omico.xero.ui.theme
 import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -13,49 +14,21 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import com.example.myapplication.ui.theme.Typography
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun XeroTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    systemUiController: SystemUiController = rememberSystemUiController(),
     dynamicColor: Boolean = true,
+    colorScheme: ColorScheme = xeroColorScheme(
+        darkTheme = darkTheme,
+        dynamicColor = dynamicColor,
+    ),
+    systemUiController: SystemUiController = rememberSystemUiController(),
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && isDynamicColorSupported -> when {
-            darkTheme -> dynamicDarkColorScheme(LocalContext.current)
-            else -> dynamicLightColorScheme(LocalContext.current)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
+    if (!LocalView.current.isInEditMode) {
         SideEffect {
             systemUiController.setSystemBarsColor(
                 color = Color.Transparent,
@@ -71,6 +44,83 @@ fun XeroTheme(
     )
 }
 
-val isDynamicColorSupported: Boolean
+inline val isDynamicColorSupported: Boolean
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
     get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+@Composable
+private fun xeroColorScheme(
+    darkTheme: Boolean,
+    dynamicColor: Boolean,
+) = when {
+    dynamicColor && isDynamicColorSupported -> when {
+        darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        else -> dynamicLightColorScheme(LocalContext.current)
+    }
+    darkTheme -> DarkColorScheme
+    else -> LightColorScheme
+}
+
+private val LightColorScheme = lightColorScheme(
+    primary = lightPrimary,
+    onPrimary = lightOnPrimary,
+    primaryContainer = lightPrimaryContainer,
+    onPrimaryContainer = lightOnPrimaryContainer,
+    secondary = lightSecondary,
+    onSecondary = lightOnSecondary,
+    secondaryContainer = lightSecondaryContainer,
+    onSecondaryContainer = lightOnSecondaryContainer,
+    tertiary = lightTertiary,
+    onTertiary = lightOnTertiary,
+    tertiaryContainer = lightTertiaryContainer,
+    onTertiaryContainer = lightTnTertiaryContainer,
+    error = lightError,
+    errorContainer = lightErrorContainer,
+    onError = lightOnError,
+    onErrorContainer = lightOnErrorContainer,
+    background = lightBackground,
+    onBackground = lightOnBackground,
+    surface = lightSurface,
+    onSurface = lightOnSurface,
+    surfaceVariant = lightSurfaceVariant,
+    onSurfaceVariant = lightOnSurfaceVariant,
+    outline = lightOutline,
+    inverseOnSurface = lightInverseOnSurface,
+    inverseSurface = lightInverseSurface,
+    inversePrimary = lightInversePrimary,
+    surfaceTint = lightSurfaceTint,
+    outlineVariant = lightOutlineVariant,
+    scrim = lightScrim,
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = darkPrimary,
+    onPrimary = darkOnPrimary,
+    primaryContainer = darkPrimaryContainer,
+    onPrimaryContainer = darkOnPrimaryContainer,
+    secondary = darkSecondary,
+    onSecondary = darkOnSecondary,
+    secondaryContainer = darkSecondaryContainer,
+    onSecondaryContainer = darkOnSecondaryContainer,
+    tertiary = darkTertiary,
+    onTertiary = darkOnTertiary,
+    tertiaryContainer = darkTertiaryContainer,
+    onTertiaryContainer = darkOnTertiaryContainer,
+    error = darkError,
+    errorContainer = darkErrorContainer,
+    onError = darkOnError,
+    onErrorContainer = darkOnErrorContainer,
+    background = darkBackground,
+    onBackground = darkOnBackground,
+    surface = darkSurface,
+    onSurface = darkOnSurface,
+    surfaceVariant = darkSurfaceVariant,
+    onSurfaceVariant = darkOnSurfaceVariant,
+    outline = darkOutline,
+    inverseOnSurface = darkInverseOnSurface,
+    inverseSurface = darkInverseSurface,
+    inversePrimary = darkInversePrimary,
+    surfaceTint = darkSurfaceTint,
+    outlineVariant = darkOutlineVariant,
+    scrim = darkScrim,
+)
